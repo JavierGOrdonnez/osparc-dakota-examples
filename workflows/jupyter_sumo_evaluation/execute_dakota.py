9700d57143d758301df60ead96f102b896b01ac7
Javier Garcia Ordonez <ordonez@zmt.swiss>
2024-09-10 07:17:06 +0200

    jupyter - SUMO evaluation w Cedric data - 2

---
+++ workflows/jupyter_sumo_evaluation/execute_dakota.py
@@ -0,0 +1,30 @@
+### This file is the access point for "make python_example"
+## It will sample for a simple function - to demonstrate feasibility of the approach
+## When this is uploaded to OSPARC, the same dakota.in will be executed inside DakotaService
+## and model evaluations will be executed within the ParallelRunner
+
+import pathlib as pl
+import dakota.environment as dakenv
+import sys
+
+script_dir = pl.Path(__file__).parent
+print(script_dir)
+sys.path.append(script_dir)
+
+#
+
+
+def main():
+    dakota_conf_path = script_dir / "dakota.in"
+    dakota_conf = dakota_conf_path.read_text()
+    study = dakenv.study(
+        callbacks={},
+        input_string=dakota_conf,
+    )
+
+    study.execute()
+
+
+if __name__ == "__main__":
+
+    main()
